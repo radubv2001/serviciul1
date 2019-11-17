02a909a0ab496eec610ebbe4a67775cdaae8dbbe
Radu Grozavu <grozavu.radu@gmail.com>
2019-11-17 10:46:51 +0200

    no message

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
-echo 'Hello world !!!!';
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Set session variables
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+echo "Session variables are set.";
+?>
+
+</body>
+</html>
